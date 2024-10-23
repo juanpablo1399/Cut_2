89392f929fdf3cb96fcb8baaa535f7d5f44e626a
Juan Pablo Gutierrez <juanpablo9913@outlook.com>
2024-10-22 22:58:46 -0500

    first commit

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from app.routes import router
+from pydantic import BaseModel, Field
+from datetime import date
+
+# Esta clase ya se ha definido en models.py, por lo que se puede eliminar de aquí.
+# Si es necesario usarla, se debe importar desde models.py
+from app.models import CourseCreate
+
+app = FastAPI()
+
+app.include_router(router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"} 
